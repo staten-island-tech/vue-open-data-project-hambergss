73a13bc817128ed8961b7eda5aef0c9dd883a760
hambergss <144480774+hambergss@users.noreply.github.com>
2025-03-10 14:02:12 -0400

    d

--- app/src/components/ApiData.vue
+++ app/src/components/ApiData.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
+
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
-const pokemon = ref('')
-async function getPokemon() {
+const doeInfo = ref('')
+async function getDoeInfo() {
   const res = await fetch(`https://data.cityofnewyork.us/resource/uq7m-95z8.json`)
   const data = await res.json()
   doeInfo.value = data
