b54915af6bdaab0e48e5040bd67bba7a339373a6
hambergss <144480774+hambergss@users.noreply.github.com>
2025-03-18 14:02:08 -0400

    router

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    
+    <RouterLink />
   </div>
 </template>
 
 <script setup>
-
+import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <style scoped>
---
+++ app/src/components/AreaChart.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <canvas ref="chartCanvas"></canvas>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, watch } from 'vue';
+import Chart from 'chart.js/auto';
+
+const chartCanvas = ref(null);
+let chartInstance = null;
+
+
+</script>
+
+<style scoped></style>
